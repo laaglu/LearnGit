179a6ff188a47ebb3e31be39abb6082f00b72ed6
Lukas Laag <laaglu@gmail.com>
2012-03-10 19:59:24 +0100

    More jenkin tests

--- src/main/java/org/vectomatic/App.java
+++ src/main/java/org/vectomatic/App.java
@@ -2,6 +2,10 @@ package org.vectomatic;
 
 import java.util.Arrays;
 
+/**
+ * Application main class.
+ * @author laaglu
+ */
 public class App {
 	public static void main(String[] args) {
 		System.out.print("org.vectomatic.App: ");
