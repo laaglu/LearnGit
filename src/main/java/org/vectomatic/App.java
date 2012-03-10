c3744345df1d4db1516aa7924d1e1c8fc8f9ac3b
Lukas Laag <laaglu@gmail.com>
2012-03-10 19:47:20 +0100

    A change to test if jenkins picks it up automatically

--- src/main/java/org/vectomatic/App.java
+++ src/main/java/org/vectomatic/App.java
@@ -2,12 +2,9 @@ package org.vectomatic;
 
 import java.util.Arrays;
 
-/**
- * Hello world!
- * 
- */
 public class App {
 	public static void main(String[] args) {
+		System.out.print("org.vectomatic.App: ");
 		System.out.println(Arrays.asList(args));
 	}
 }
